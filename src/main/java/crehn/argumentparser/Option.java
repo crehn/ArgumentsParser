60718ea58e047e27ccf68461ea5af713526505b1
Christian Rehn <mail@christian-rehn.de>
2013-12-08 22:35:34 +0100

    Option.parse()

--- src/main/java/crehn/argumentparser/Option.java
+++ src/main/java/crehn/argumentparser/Option.java
@@ -6,15 +6,20 @@ import lombok.Data;
 public class Option
 {
 	private final char name;
-	private boolean isSet = false;
+	private Boolean isSet = null;
 	
-	public void parse(String string)
+	public void parse(String argument)
 	{
-		
+		isSet = optionString().equals(argument);
+	}
+
+	private String optionString()
+	{
+		return "-" + name;
 	}
 	
 	public boolean canHandle(String argument)
 	{
-		return ("-" + name).equals(argument);
+		return optionString().equals(argument);
 	}
 }
--- src/test/java/crehn/argumentparser/OptionTest.java
+++ src/test/java/crehn/argumentparser/OptionTest.java
@@ -1,41 +1,79 @@
 package crehn.argumentparser;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class OptionTest
 {
+	private Option option;
+	
+	@Before
+	public void setup()
+	{
+		option = new Option('o');
+	}
+	
+	@Test
+	public void initiallyValueIsUnknown() throws Exception
+	{
+		assertNull(option.getIsSet());
+	}
+	
 	@Test
 	public void canHandleReturnsFalseForEmptyString() throws Exception
 	{
-		Option option = new Option('o');
-		
 		assertFalse(option.canHandle(""));
 	}
 	
 	@Test
 	public void canHandleReturnsFalseWhenDashIsMissing() throws Exception
 	{
-		Option option = new Option('o');
-		
 		assertFalse(option.canHandle("o"));
 	}
 	
 	@Test
 	public void canHandleReturnsFalseWhenOtherChar() throws Exception
 	{
-		Option option = new Option('o');
-		
 		assertFalse(option.canHandle("-u"));
 	}
 	
 	@Test
 	public void canHandleReturnsTrue() throws Exception
 	{
-		Option option = new Option('o');
-		
 		assertTrue(option.canHandle("-o"));
+	}
+	
+	@Test
+	public void parseParsesEmptyString() throws Exception
+	{
+		option.parse("");
+		
+		assertFalse(option.getIsSet());
+	}
+	
+	@Test
+	public void parseParsesOption() throws Exception
+	{
+		option.parse("-o");
+		
+		assertTrue(option.getIsSet());
+	}
+	
+	@Test
+	public void parseParsesOtherOption() throws Exception
+	{
+		option.parse("-u");
+		
+		assertFalse(option.getIsSet());
+	}
+	
+	@Test
+	public void parseParsesMissingDash() throws Exception
+	{
+		option.parse("o");
+		
+		assertFalse(option.getIsSet());
 	}
 }
