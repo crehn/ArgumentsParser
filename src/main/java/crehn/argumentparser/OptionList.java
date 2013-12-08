d34023d65a35f6964fc7168a4d6371ba4c46f5d9
Christian Rehn <mail@christian-rehn.de>
2013-12-08 22:22:43 +0100

    simplified OptionList.parse() by using recursion

--- src/main/java/crehn/argumentparser/OptionList.java
+++ src/main/java/crehn/argumentparser/OptionList.java
@@ -1,6 +1,7 @@
 package crehn.argumentparser;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OptionList extends ArrayList<Option>
 {
@@ -13,23 +14,22 @@ public class OptionList extends ArrayList<Option>
 	
 	public List<String> parse(List<String> yetToParse)
 	{
-		if (yetToParse.size() == 0)
-			return Collections.emptyList();
+		Option option = getOptionByArgument(yetToParse);
+		if (option == null)
+			return yetToParse;
 		
-		Option option = null;
-		while (yetToParse.size() > 0 && (option = getOptionByArgument(yetToParse.get(0))) != null)
-		{
-			option.parse(yetToParse.get(0));
-			yetToParse = yetToParse.subList(1, yetToParse.size());
-		}
-		return yetToParse;
+		option.parse(yetToParse.get(0));
+		return parse(yetToParse.subList(1, yetToParse.size()));
 	}
 	
-	private Option getOptionByArgument(String argument)
+	private Option getOptionByArgument(List<String> yetToParse)
 	{
+		if (yetToParse.size() == 0)
+			return null;
+		
 		for (Option option : this)
 		{
-			if (option.canHandle(argument))
+			if (option.canHandle(yetToParse.get(0)))
 				return option;
 		}
 		return null;
