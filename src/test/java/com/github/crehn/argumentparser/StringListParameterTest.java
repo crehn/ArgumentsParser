1d72aa000931d058f801547992a0f156289fde93
Christian Rehn <mail@christian-rehn.de>
2014-02-01 11:23:44 +0100

    improved test coverage

---
+++ src/test/java/com/github/crehn/argumentparser/StringListParameterTest.java
@@ -0,0 +1,33 @@
+package com.github.crehn.argumentparser;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StringListParameterTest {
+	
+	private StringListParameter param;
+	
+	@Before
+	public void setup() {
+		param = new StringListParameter('p');
+	}
+	
+	@Test
+	public void canHandleReturnsFalseForEmptyList() throws Exception {
+		assertFalse(param.canHandle(Collections.<String> emptyList()));
+	}
+	
+	@Test
+	public void parseReturnsIdentityWhenCannotHanlde() throws Exception {
+		List<String> yetToParse = param.parse(asList("-q"));
+		
+		assertEquals(asList("-q"), yetToParse);
+	}
+}
